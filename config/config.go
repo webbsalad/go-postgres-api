7e94c3b245f39121eea26566731b7848dede8056
webbsalad <webbbsalad@gmail.com>
2024-05-26 00:15:39 +0300

    -

--- config/config.go
+++ config/config.go
@@ -4,24 +4,23 @@ import (
 	"log"
 
 	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/joho/godotenv"
 )
 
 type ConfigDatabase struct {
 	Port     string `env:"DB_PORT"`
-	Host     string `env:"DB_HOST" `
-	Name     string `env:"DB_NAME" `
+	Host     string `env:"DB_HOST"`
+	Name     string `env:"DB_NAME"`
 	User     string `env:"DB_USER"`
 	Password string `env:"DB_PASSWORD"`
 }
 
 func LoadConfig() (ConfigDatabase, error) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
-
 	var cfg ConfigDatabase
-	err = cleanenv.ReadEnv(&cfg)
+	err := cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		log.Println("Error reading environment variables")
+	} else {
+		log.Printf("Loaded config: %#v\n", cfg)
+	}
 	return cfg, err
 }
